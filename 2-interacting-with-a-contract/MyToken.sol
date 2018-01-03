6e87fb1ea0d6f83d84b9ae2b6d46b79285acff02
Yepcoin <fengyaowei@outlook.com>
2018-01-03 16:03:09 +0800

    Update MyToken.sol

--- 2-interacting-with-a-contract/MyToken.sol
+++ 2-interacting-with-a-contract/MyToken.sol
@@ -1,32 +1,35 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.4; // 告知solidity的版本
 
 contract MyToken {
-  string public constant name = "MyToken";
-  string public constant symbol = "MTK";
-  uint256 public constant decimals = 18;
-  uint256 public constant INITIAL_SUPPLY = 10000;
+  string public constant name = "MyToken"; 
+  string public constant symbol = "MTK"; // 类似RMB
+  uint256 public constant decimals = 18; // 精确度
+  uint256 public constant INITIAL_SUPPLY = 10000;// 初始代币量
 
-  address public creator;
-  uint256 public totalSupply;
-  mapping (address => uint256) public balances;
+  address public creator; // 持有人
+  uint256 public totalSupply; // 总供给量
+  mapping (address => uint256) public balances;// 参与人的余量
 
+  // 初始化、只执行一次，定义了持有人为合约发起人，总量1万，持有人全部拥有1万代币
   function MyToken() public {
     creator = msg.sender;
     totalSupply = INITIAL_SUPPLY;
     balances[creator] = INITIAL_SUPPLY;
   }
 
+  // 发送代币
   function sendTokens(address receiver, uint256 amount) public returns (bool) {
-    address owner = msg.sender;
+    address owner = msg.sender; // 调用方即为参与人
 
-    require(amount > 0);
-    require(balances[owner] >= amount);
+    require(amount > 0); // 参与人资格验证-有代币
+    require(balances[owner] >= amount);// 参与人资格验证-有足够代币
 
-    balances[owner] -= amount;
-    balances[receiver] += amount;
-    return true;
+    balances[owner] -= amount; // 发起方减少代币
+    balances[receiver] += amount;// 接收方增加大笔
+    return true;// 完成转移交易代币过程
   }
-
+  
+  // 查看账户的代币余额
   function balanceOf(address owner) public constant returns (uint256) {
     return balances[owner];
   }
